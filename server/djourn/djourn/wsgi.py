c3ca20eff48c06bebd1246d2390356ef3854fcfa
lightyaer <mmnyed@gmail.com>
2020-09-01 02:50:36 +0530

    Setup linter and formatter

--- server/djourn/djourn/wsgi.py
+++ server/djourn/djourn/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djourn.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djourn.settings")
 
 application = get_wsgi_application()
